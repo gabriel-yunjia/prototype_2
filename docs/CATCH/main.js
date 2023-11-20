71bb8d3d874b2da045b41cd03c9c5dba9ca511f8
gabriel-yunjia <gli79@ucsc.edu>
2023-11-20 14:32:53 -0800

    fixed

--- docs/CATCH/main.js
+++ docs/CATCH/main.js
@@ -1,22 +1,16 @@
 title = "CATCH";
-
 description = `
 [Move] Move Cursor To Move Pan
 `;
-
 characters = [`
    p|====/
-
-
 `];
-
 options = {
   theme: "simple",
   viewSize: { x: 300, y: 300 },
   isPlayingBgm: true,
   isReplayEnabled: true,
 };
-
 /** @type {{ pos: Vector, char: number }} */
 let player;
 let speed;
@@ -24,7 +18,6 @@ let strikes;
 let foods;
 let bombs;
 let colors;
-
 function update() {
   // Initialize game elements
   if (!ticks) {
@@ -34,17 +27,13 @@ function update() {
     foods = [];
     bombs = [];
   }
-
   // Draw boundaries
   color("yellow");
   rect(0, 0, 7, 300);
   rect(293, 0, 7, 300);
-
-
   // Draw player character
   color("black");
   char(characters[player.char], player.pos);
-
   switch(strikes){
     case 0:
     colors = "green"
@@ -59,12 +48,9 @@ function update() {
   
   color(colors);
   let playerCollider = box(player.pos.x + 40,player.pos.y, 50,10); // Adjust the size as needed
-
   // Move player automatically
   player.pos.x = clamp(input.pos.x-35, -10, 230);
-
   
-
   // Generate food
   if (rnd() < 0.02) {
     foods.push({
@@ -72,33 +58,27 @@ function update() {
       speed: rnd(1, 3),
     });
   }
-
   // Draw and move food
   color("purple");
   remove(foods, (food) => {
     food.pos.y += food.speed;
-
     // Check collision with player
     if (box(food.pos, 1).isColliding.rect.green || box(food.pos, 1).isColliding.rect.yellow || box(food.pos, 1).isColliding.rect.red) {
       play("coin");
       addScore(2);
       return true; // Remove the collided food only
     }
-
     // Check if food missed
     if (food.pos.y > 300) {
       play("hit");
       addScore(-1);
       return true;
     }
-
     // Draw food
     
     box(food.pos, 9);
     return false;
   });
-
-
     // Generate BOMBS
   if (rnd() < 0.01) {
       bombs.push({
@@ -121,7 +101,6 @@ function update() {
   
       // Check if food missed
       if (bomb.pos.y > 300) {
-
         return true;
       }
   
@@ -130,14 +109,10 @@ function update() {
       box(bomb.pos, 8);
       return false;
     });
-
-
   // Check game over conditions
   if (strikes >= 3) {
     play("hit");
     end();
   }
   
-
-
 }
